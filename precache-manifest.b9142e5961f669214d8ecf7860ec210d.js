03820d96a8da25acfb8981c7be851c281967d929
Munaf <muhammadmunaf>
2022-07-24 22:00:55 +0500

    Updates

---
+++ precache-manifest.b9142e5961f669214d8ecf7860ec210d.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "71398745f24d4cc0d588",
+    "url": "/youtube/static/css/main.3922f723.chunk.css"
+  },
+  {
+    "revision": "71398745f24d4cc0d588",
+    "url": "/youtube/static/js/main.71398745.chunk.js"
+  },
+  {
+    "revision": "65cd119ab16b9673c385",
+    "url": "/youtube/static/js/1.65cd119a.chunk.js"
+  },
+  {
+    "revision": "9e3ea2a4fd8ddf97340f",
+    "url": "/youtube/static/js/runtime~main.9e3ea2a4.js"
+  },
+  {
+    "revision": "9ef10233c5bb7ba0b2890fd839888610",
+    "url": "/youtube/static/media/watchLater.9ef10233.svg"
+  },
+  {
+    "revision": "ece91bbed0d7cb29ac0dd2adbcb4506a",
+    "url": "/youtube/static/media/report.ece91bbe.svg"
+  },
+  {
+    "revision": "cecd8a51a2eefe6505fdcbe7c9c4713d",
+    "url": "/youtube/static/media/arrowBottom.cecd8a51.svg"
+  },
+  {
+    "revision": "d2ff1176e585e3db3fe71eb080c8ff22",
+    "url": "/youtube/static/media/search.d2ff1176.svg"
+  },
+  {
+    "revision": "fa4680fd9391b147bb9d4e62670fa3ff",
+    "url": "/youtube/static/media/mic.fa4680fd.svg"
+  },
+  {
+    "revision": "1d5169ce9da560cd605a4a0c6fd2f655",
+    "url": "/youtube/static/media/hamburger.1d5169ce.svg"
+  },
+  {
+    "revision": "cca2aa9dffd9a41c913c130b526ec3af",
+    "url": "/youtube/static/media/create.cca2aa9d.svg"
+  },
+  {
+    "revision": "7bcd2e178f4f8f33cd322ba2ee2dd099",
+    "url": "/youtube/static/media/notifications.7bcd2e17.svg"
+  },
+  {
+    "revision": "c7790f2604410a8176378e600ef501b5",
+    "url": "/youtube/static/media/queue.c7790f26.svg"
+  },
+  {
+    "revision": "eba95a413a2565e8088f6d8902e9be50",
+    "url": "/youtube/static/media/Save.eba95a41.svg"
+  },
+  {
+    "revision": "57e3ee24bf8da13ba493a3551c864ab4",
+    "url": "/youtube/static/media/circleCross.57e3ee24.svg"
+  },
+  {
+    "revision": "5e2e0693957c0cff2fcae6ae59d5e1cc",
+    "url": "/youtube/static/media/circleCut.5e2e0693.svg"
+  },
+  {
+    "revision": "22051605be001070492929f29aa0241b",
+    "url": "/youtube/static/media/home.22051605.svg"
+  },
+  {
+    "revision": "2386053ba0d5367cf0d5805ba5c51a15",
+    "url": "/youtube/static/media/explore.2386053b.svg"
+  },
+  {
+    "revision": "1cc4079b483307eec94ad78bf1cb4fa3",
+    "url": "/youtube/static/media/subscriptions.1cc4079b.svg"
+  },
+  {
+    "revision": "140883b96b4b446493fc66b9e9b63a3d",
+    "url": "/youtube/static/media/library.140883b9.svg"
+  },
+  {
+    "revision": "40a30faee8281b5a4da3a96f28941da1",
+    "url": "/youtube/static/media/history.40a30fae.svg"
+  },
+  {
+    "revision": "d2b3831bf153125a3e2c82b166490a77",
+    "url": "/youtube/static/media/yourVideos.d2b3831b.svg"
+  },
+  {
+    "revision": "3c99db5fc41316ae58857d1fd95fca05",
+    "url": "/youtube/static/media/liked.3c99db5f.svg"
+  },
+  {
+    "revision": "8921e25a3de1a0907b27247368e64e87",
+    "url": "/youtube/static/media/sports.8921e25a.svg"
+  },
+  {
+    "revision": "c1c32c2b00fe44e199e6bfa5506d833f",
+    "url": "/youtube/static/media/gaming.c1c32c2b.svg"
+  },
+  {
+    "revision": "a3dc66063c2327f55451564f694fdb66",
+    "url": "/youtube/static/media/live.a3dc6606.svg"
+  },
+  {
+    "revision": "aa949f459424972966f8250ac2f9e48d",
+    "url": "/youtube/static/media/youtubeCreator.aa949f45.svg"
+  },
+  {
+    "revision": "1950675ade580af470858c2d1f77d9a8",
+    "url": "/youtube/static/media/youtubeMusic.1950675a.svg"
+  },
+  {
+    "revision": "e13ac5289a1a093a435876211ec173b9",
+    "url": "/youtube/static/media/youtubeKids.e13ac528.svg"
+  },
+  {
+    "revision": "6116e621c7e183a6aed54199535eedde",
+    "url": "/youtube/static/media/youtubeTV.6116e621.svg"
+  },
+  {
+    "revision": "c3a3b2a31cb454c880528d6a023dac0c",
+    "url": "/youtube/static/media/settings.c3a3b2a3.svg"
+  },
+  {
+    "revision": "3d4aa154194f0bbdd53567936c60c201",
+    "url": "/youtube/static/media/help.3d4aa154.svg"
+  },
+  {
+    "revision": "f26e541df6fa7b52073122ac9394ee6f",
+    "url": "/youtube/static/media/feedback.f26e541d.svg"
+  },
+  {
+    "revision": "fae6a9aa8a161e2bbe614ef53e2e2edc",
+    "url": "/youtube/index.html"
+  }
+];
